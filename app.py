b73702cda135ed32cfffa8c339b2aaca3b0ddb4f
Kyrillos Abdelshaheed <kyrillos.abdelshaheed.22@cnu.edu>
2025-05-30 11:03:59 -0400

    desktop bugs and future proofing

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
+import os
+
 from flask import Flask, render_template, request
 import Prices
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+load_dotevn()
 
-@app.route("/")
-def home():
-    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    app.db = client.Gnarly_bros
+    @app.route("/")
+    def home():
+        return render_template("home.html")
 
-@app.route("/services/", methods=["GET", "POST"])
-def services():
-    interior_price, exterior_price, both = 80,40,100
-    selected_model = 'Sedan'
-    if request.method == "POST":
-        selected_model = request.form.get('model')
-        prices = Prices.Prices(selected_model)
-        interior_price, exterior_price, both = prices.get_price()
-    return render_template("services.html", selected_model=selected_model,interior_price=interior_price, exterior_price=exterior_price, both=both)
+    @app.route("/about/")
+    def about():
+        return render_template("about.html")
 
-@app.route("/booknow/")
-def booknow():
-    return render_template("booknow.html")
+    @app.route("/services/", methods=["GET", "POST"])
+    def services():
+        interior_price, exterior_price, both = 80,40,100
+        selected_model = 'Sedan'
+        if request.method == "POST":
+            selected_model = request.form.get('model')
+            prices = Prices.Prices(selected_model)
+            interior_price, exterior_price, both = prices.get_price()
+        return render_template("services.html", selected_model=selected_model,interior_price=interior_price, exterior_price=exterior_price, both=both)
 
-@app.route("/socials/")
-def socials():
-    return render_template("socials.html")
+    @app.route("/booknow/")
+    def booknow():
+        return render_template("booknow.html")
+
+    @app.route("/socials/")
+    def socials():
+        return render_template("socials.html")
+
+    return app
 
 if __name__ == "__main__":
     app.run()
