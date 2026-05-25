911a2d40a226c6ce78118a5c6e2317215a499cd4
Kyrillos Abdelshaheed <kyrillos.abdelshaheed.22@cnu.edu>
2026-05-24 20:42:28 -0400

    updated price via cooneys request

--- Prices.py
+++ Prices.py
@@ -4,37 +4,37 @@ class Prices:
         self.model = model
         self.interior = 150
         self.exterior = 100
-        self.both = 200
-        self.claybar = 40
+        self.both = 125
+        self.claybar = 30
         self.polish = 120
-        self.ceramic = 40
+        self.ceramic = 50
         self.carpet = 80
 
     def get_price(self):
         if self.model == "Sedan":
             self.interior = 150
             self.exterior = 100
-            self.both = 200
+            self.both = 125
             self.carpet = 80
         elif self.model == "SUV":
             self.interior = 170
             self.exterior = 120
-            self.both = 240
+            self.both = 150
             self.carpet = 100
         elif self.model == "LargeSUV":
             self.interior = 200
             self.exterior = 150
-            self.both = 300
+            self.both = 170
             self.carpet = 120
         elif self.model == "Truck":
             self.interior = 170
             self.exterior = 140
-            self.both = 250
+            self.both = 150
             self.carpet = 100
         else:
             self.interior = 170
             self.exterior = 150
-            self.both = 270
+            self.both = 170
             self.carpet = 100
 
         return self.interior, self.exterior, self.both, self.claybar, self.ceramic, self.polish, self.carpet
